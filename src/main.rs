0481d35e4407451916f83c8195ebabbf97a07a79
Rene Stein <rene@renestein.net>
2021-07-27 13:37:30 +0200

    Better main.rs structure

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,9 @@ use std::collections;
 use std::io;
 
 fn main() {
-    let mut ascii_table = get_ascii_table();
-    print_char_table(&ascii_table);
+    
     let mut config = PlayfairConfiguration::new();
-
     println!("{}", config);
-    ascii_table.retain(|c| *c != config.unused_char);
-    print_char_table(&ascii_table);
 
     loop {
         let mut choice = String::new();
@@ -56,6 +52,155 @@ fn main() {
 
         break;
     }
+
+    let mut raw_input_text = String::new();
+    if config.is_encrypt() {
+        println!("Input your plain text and press <Enter>:");
+    } else {
+        println!("Input your cipher text and press <Enter>:");
+    }
+
+    let read_result = io::stdin().read_line(&mut raw_input_text);
+    match read_result {
+        Ok(_) => {
+            raw_input_text = raw_input_text.to_uppercase();        
+        }
+
+        Err(error) => panic!("Unexpected error while reading input data {}.", error),
+    }
+
+    let output_text = encrypt_decrypt(&raw_input_text, &config);
+    println!("Output text:");
+    println!("");
+    println!("{}", output_text);
+}
+
+fn encrypt_decrypt(raw_input_text: &str, config: &PlayfairConfiguration) -> String {
+
+    let (key_table, ascii_table) = generate_key_table(config);
+    let input_text = prepare_input_text(raw_input_text, &ascii_table, config);
+    let mut output_text = String::with_capacity(input_text.len());
+    let mut first_char = config.unused_char;
+
+    for input_char in input_text.chars() {
+        if first_char == config.unused_char {
+            first_char = input_char;
+            continue;
+        }
+
+        let first_char_row = key_table
+            .iter()
+            .position(|row| row.contains(&first_char))
+            .unwrap();
+        let first_char_column = key_table[first_char_row]
+            .iter()
+            .position(|c| first_char == *c)
+            .unwrap();
+        let second_char_row = key_table
+            .iter()
+            .position(|row| row.contains(&input_char))
+            .unwrap();
+        let second_char_column = key_table[second_char_row]
+            .iter()
+            .position(|c| input_char == *c)
+            .unwrap();
+
+        let add_to_same_row_or_column = {
+            if config.is_decrypt() {
+                KEY_ROWS - 1
+            } else {
+                1
+            }
+        };
+
+        match (
+            first_char_row == second_char_row,
+            first_char_column == second_char_column,
+        ) {
+            (true, false) => {
+                output_text.push(
+                    key_table[first_char_row]
+                        [(first_char_column + add_to_same_row_or_column) % KEY_COLUMNS],
+                );
+                output_text.push(
+                    key_table[second_char_row]
+                        [(second_char_column + add_to_same_row_or_column) % KEY_COLUMNS],
+                )
+            }
+            (false, true) => {
+                output_text.push(
+                    key_table[(first_char_row + add_to_same_row_or_column) % KEY_ROWS]
+                        [first_char_column],
+                );
+                output_text.push(
+                    key_table[(second_char_row + add_to_same_row_or_column) % KEY_ROWS]
+                        [(second_char_column)],
+                );
+            }
+            (_, _) => {
+                output_text.push(key_table[first_char_row][second_char_column]);
+                output_text.push(key_table[second_char_row][first_char_column]);
+            }
+        }
+
+        first_char = config.unused_char;
+    }
+
+    return output_text;
+}
+
+fn prepare_input_text(
+    raw_input_text: &str,
+    ascii_table: &[char],
+    config: &PlayfairConfiguration) -> String {
+
+    let mut output_text = String::with_capacity(raw_input_text.len() + 1);
+    let mut first_char = config.unused_char;
+    for elem in raw_input_text.chars() {
+        if !ascii_table.contains(&elem) && elem != config.unused_char {
+            continue;
+        }
+
+        if config.is_decrypt() {
+            output_text.push(elem);
+            continue;
+        }
+
+        let plain_text_char = {
+            if elem == config.unused_char {
+                config.replace_char_for_unused_char
+            } else {
+                elem
+            }
+        };
+
+        if first_char != config.unused_char {
+            output_text.push(first_char);
+            if first_char == plain_text_char {
+                output_text.push(config.surrogate_char);
+            }
+            output_text.push(plain_text_char);
+            first_char = config.unused_char;
+        } else {
+            first_char = plain_text_char;
+        }
+    }
+
+    if config.is_encrypt() {
+        if first_char != config.unused_char {
+            output_text.push(first_char);
+            output_text.push(config.surrogate_char);
+        }
+    }
+
+    return output_text;
+}
+
+fn generate_key_table(config: &PlayfairConfiguration) -> (Vec<Vec<char>>, Vec<char>) {
+
+    let mut ascii_table = get_ascii_table();
+    ascii_table.retain(|c| *c != config.unused_char);
+    print_char_table(&ascii_table);
 
     let mut key_table = vec![vec!['0'; KEY_COLUMNS]; KEY_ROWS];
     let mut current_key_table_index_0 = 0;
@@ -103,150 +248,8 @@ fn main() {
         print_char_table(&key_table[row]);
     }
 
-    let mut raw_input_text = String::new();
-    let input_text: String;
-    if config.is_encrypt() {
-        println!("Input your plain text and press <Enter>:");
-    } else {
-        println!("Input your cipher text and press <Enter>:");
-    }
-
-    let read_result = io::stdin().read_line(&mut raw_input_text);
-    match read_result {
-        Ok(_) => {
-            raw_input_text = raw_input_text.to_uppercase();
-            input_text = prepare_input_text(&raw_input_text, &ascii_table, &config)
-        }
-
-        Err(error) => panic!("Unexpected error while reading input data {}.", error),
-    }
-
-    let output_text = encrypt_decrypt(&input_text, &key_table, &config);
-    println!("Output text:");
-    println!("");
-    println!("{}", output_text);
-}
-
-fn encrypt_decrypt(
-    input_text: &str,
-    keytable: &[Vec<char>],
-    config: &PlayfairConfiguration,
-) -> String {
-    let mut output_text = String::with_capacity(input_text.len());
-    let mut first_char = config.unused_char;
-
-    for input_char in input_text.chars() {
-        if first_char == config.unused_char {
-            first_char = input_char;
-            continue;
-        }
-
-        let first_char_row = keytable
-            .iter()
-            .position(|row| row.contains(&first_char))
-            .unwrap();
-        let first_char_column = keytable[first_char_row]
-            .iter()
-            .position(|c| first_char == *c)
-            .unwrap();
-        let second_char_row = keytable
-            .iter()
-            .position(|row| row.contains(&input_char))
-            .unwrap();
-        let second_char_column = keytable[second_char_row]
-            .iter()
-            .position(|c| input_char == *c)
-            .unwrap();
-
-        let add_to_same_row_or_column = {
-            if config.is_decrypt() {
-                KEY_ROWS - 1
-            } else {
-                1
-            }
-        };
-
-        match (
-            first_char_row == second_char_row,
-            first_char_column == second_char_column,
-        ) {
-            (true, false) => {
-                output_text.push(
-                    keytable[first_char_row]
-                        [(first_char_column + add_to_same_row_or_column) % KEY_COLUMNS],
-                );
-                output_text.push(
-                    keytable[second_char_row]
-                        [(second_char_column + add_to_same_row_or_column) % KEY_COLUMNS],
-                )
-            }
-            (false, true) => {
-                output_text.push(
-                    keytable[(first_char_row + add_to_same_row_or_column) % KEY_ROWS]
-                        [first_char_column],
-                );
-                output_text.push(
-                    keytable[(second_char_row + add_to_same_row_or_column) % KEY_ROWS]
-                        [(second_char_column)],
-                );
-            }
-            (_, _) => {
-                output_text.push(keytable[first_char_row][second_char_column]);
-                output_text.push(keytable[second_char_row][first_char_column]);
-            }
-        }
-
-        first_char = config.unused_char;
-    }
-
-    return output_text;
-}
-
-fn prepare_input_text(
-    raw_input_text: &str,
-    ascii_table: &[char],
-    config: &PlayfairConfiguration,
-) -> String {
-    let mut output_text = String::with_capacity(raw_input_text.len() + 1);
-    let mut first_char = config.unused_char;
-    for elem in raw_input_text.chars() {
-        if !ascii_table.contains(&elem) && elem != config.unused_char {
-            continue;
-        }
-
-        if config.is_decrypt() {
-            output_text.push(elem);
-            continue;
-        }
-
-        let plain_text_char = {
-            if elem == config.unused_char {
-                config.replace_char_for_unused_char
-            } else {
-                elem
-            }
-        };
-
-        if first_char != config.unused_char {
-            output_text.push(first_char);
-            if first_char == plain_text_char {
-                output_text.push(config.surrogate_char);
-            }
-            output_text.push(plain_text_char);
-            first_char = config.unused_char;
-        } else {
-            first_char = plain_text_char;
-        }
-    }
-
-    if config.is_encrypt() {
-        if first_char != config.unused_char {
-            output_text.push(first_char);
-            output_text.push(config.surrogate_char);
-        }
-    }
-
-    return output_text;
+    (key_table, ascii_table)
+    
 }
 
 fn get_ascii_table() -> Vec<char> {
